c4d84654f2c02dedfc2b67d97b3950097c65e66d
RazvanNan <razvannan@yahoo.com>
2019-07-10 12:25:47 +0100

    Add runApp_AuthTut.sh bash for running a demo on the App

--- readme.md
+++ readme.md
@@ -4,4 +4,12 @@
 This code is from here: https://medium.com/@evangow/server-authentication-basics-express-sessions-passport-and-curl-359b7456003d
 
 Note, in the session configuration below, I am leaving the ‘secret’ as ‘keyboard cat’, but in production you would want to replace this with a randomly generated string that’s pulled from an environment variable.
+<br>
 
+### To run a demo (both servers and client), run 
+    ./runApp_AuthTut.sh
+
+Make sure the demo file is made executable with 
+```bash
+chmod a+x runApp_AuthTut.sh
+```
---
+++ runApp_AuthTut.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Run in parallel the DB server, Node server and the curl commands
+
+#Explanation for the below:
+# > /dev/null: redirects the output of command(stdout) to /dev/null
+# 2>&1: redirects stderr to stdout, so errors (if any) also goes to /dev/null
+# & send a process to background
+
+# DB Server
+db () {
+  cd ./db
+  npm run json:server > /dev/null 2>&1 &
+  P1=$!
+}
+
+#Node Server
+server () {
+  cd ../server
+  npm run dev:server > /dev/null 2>&1 &
+  P2=$!
+}
+
+#Client cURL
+client () {
+  clear
+  echo
+  echo "Pls press Enter for starting client command (cURL)."
+  read
+  curl http://localhost:3000/login -c cookie-file.txt -H 'Content-Type: application/json' -d '{"email":"test@test.com", "password":"password"}' -L
+}
+
+
+db
+echo "DB started......"
+pwd
+
+server
+echo "Server started...."
+pwd
+
+sleep 3
+client
+
+echo
+echo "Waiting now.... pls press ^C for stopping DB and Node servers."
+wait $P1 $P2
